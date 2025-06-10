3d4aa58713eaa7bd2c1aaf5370db994eac5203b2
Archana Nayak <archananayak212002@gmail.com>
2025-06-10 22:24:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+// No JavaScript is required unless you want to add functionality like logging the input
+document.querySelector('.btn').addEventListener('click', () => {
+  const query = document.querySelector('.input').value;
+  console.log('Searching for:', query);
+});
+
